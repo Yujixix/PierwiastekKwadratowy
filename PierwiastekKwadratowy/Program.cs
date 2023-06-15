e3446fc6bf9cc95413dfb12669b7e534f25bd24d
Yujixix <136687825+Yujixix@users.noreply.github.com>
2023-06-15 17:09:34 +0200

    Finasl Push

--- PierwiastekKwadratowy/Program.cs
+++ PierwiastekKwadratowy/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class QuadraticEquationSolver
+{
+    public static (double? x1, double? x2) Solve(double a, double b, double c)
+    {
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant < 0)
+        {
+            return (null, null); // Brak pierwiastków rzeczywistych
+        }
+        else if (discriminant == 0)
+        {
+            double x = -b / (2 * a);
+            return (x, null); // Jeden pierwiastek rzeczywisty
+        }
+        else
+        {
+            double sqrtDiscriminant = Math.Sqrt(discriminant);
+            double x1 = (-b + sqrtDiscriminant) / (2 * a);
+            double x2 = (-b - sqrtDiscriminant) / (2 * a);
+            return (x1, x2); // Dwa pierwiastki rzeczywiste
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Program do obliczania pierwiastków równania kwadratowego");
+        Console.WriteLine("Podaj współczynniki równania ax^2 + bx + c = 0");
+
+        Console.Write("Podaj a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Podaj b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Podaj c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        (double? x1, double? x2) = Solve(a, b, c);
+
+        Console.WriteLine("Wyniki:");
+        Console.WriteLine("x1 = " + (x1.HasValue ? x1.ToString() : "brak pierwiastków rzeczywistych"));
+        Console.WriteLine("x2 = " + (x2.HasValue ? x2.ToString() : "brak pierwiastków rzeczywistych"));
+    }
+}
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,11 +1,52 @@
-namespace UnitTest
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+using Xunit;
 
-        }
+public class QuadraticEquationSolverTests
+{
+    [Fact]
+    public void NoRealRoots()
+    {
+        // Arrange
+        double a = 2;
+        double b = 4;
+        double c = 6;
+
+        // Act
+        (double? x1, double? x2) = QuadraticEquationSolver.Solve(a, b, c);
+
+        // Assert
+        Assert.Null(x1);
+        Assert.Null(x2);
+    }
+
+    [Fact]
+    public void OneRealRoot()
+    {
+        // Arrange
+        double a = 1;
+        double b = -4;
+        double c = 4;
+
+        // Act
+        (double? x1, double? x2) = QuadraticEquationSolver.Solve(a, b, c);
+
+        // Assert
+        Assert.Equal(2, x1);
+        Assert.Null(x2);
+    }
+
+    [Fact]
+    public void TwoRealRoots()
+    {
+        // Arrange
+        double a = 1;
+        double b = -3;
+        double c = -4;
+
+        // Act
+        (double? x1, double? x2) = QuadraticEquationSolver.Solve(a, b, c);
+
+        // Assert
+        Assert.Equal(4, x1);
+        Assert.Equal(-1, x2);
     }
 }
